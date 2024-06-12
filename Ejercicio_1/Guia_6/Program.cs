2627b9fa79514a3c0fd6c4f38fb88a6212e6293a
AleNotebook <alejandrovitt@hotmail.com>
2024-06-12 19:17:26 -0300

    terminamos ejercicio 1

--- Ejercicio_1/Guia_6/Program.cs
+++ Ejercicio_1/Guia_6/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,24 @@ namespace Guia_6
 {
     internal class Program
     {
+        /*
+        Ejercicio 1)
+        Un estudiante de matemáticas requiere una aplicación que permita calcular la suma de los números naturales hasta un valor determinado.Implemente una solución con C#
+        */
         static void Main(string[] args)
         {
+            int numero;
+            int suma = 0;
+
+            Console.WriteLine("Ingrese la cantidad de numeros naturales que desea sumar");
+            numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine($"La suma de los numeros hasta {numero} es de: {suma}");
+
+            Console.ReadKey();
         }
     }
 }
