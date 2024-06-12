fd5585893d1df0a0b7e03798f093023db69a635f
AleNotebook <alejandrovitt@hotmail.com>
2024-06-12 19:36:39 -0300

    fin de ejercicio 2

---
+++ Ejercicio_2/Guia_6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_6
+{
+    internal class Program
+    {
+        /*
+         * 
+         * Ejercicio 2)
+            Realizar un marco de pantalla con signos asteriscos desde la posición (5,2) a (75,22)
+
+        */
+        static void Main(string[] args)
+        {
+            string marco = "*";
+
+            int inicioX = 5;
+            int inicioY = 2;
+            int finX = 75;
+            int finY = 22;
+
+            //marco superior
+            Console.SetCursorPosition(inicioX, inicioY);
+            for (int i = inicioX; i <= finX; i++)
+            {
+                Console.Write(marco);
+            }
+
+            //Marco inferior
+            Console.SetCursorPosition(inicioX, finY);
+            for (int i = inicioX; i <= finX; i++)
+            {
+                Console.Write(marco);
+            }
+
+            for (int i = inicioY; i <= finY; i++)
+            {
+                Console.SetCursorPosition(inicioX, i);
+                Console.Write(marco);
+            }
+
+            
+
+            for (int i = inicioY; i <= finY; i++)
+            {
+                Console.SetCursorPosition(finX, i);
+                Console.Write(marco);
+            }
+            Console.ReadKey();
+        }
+    }
+}
