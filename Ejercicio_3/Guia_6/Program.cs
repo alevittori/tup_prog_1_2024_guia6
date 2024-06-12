cb1aef8b224092477f8d42e079928ebdf29ecc05
AleNotebook <alejandrovitt@hotmail.com>
2024-06-12 20:15:08 -0300

    modificaciones al ejercicio 3 y fin del ejercicio 4

--- Ejercicio_3/Guia_6/Program.cs
+++ Ejercicio_3/Guia_6/Program.cs
@@ -36,4 +36,4 @@ namespace Guia_6
             Console.ReadKey();
         }
     }
-}
+}   
---
+++ Ejercicio_4/Guia_6/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_6
+{
+    internal class Program
+    {
+
+        /*
+         * Ejercicio 4)
+            Implemente una aplicación que dados dos valores enteros ingresados por el usuario muestre en pantalla todos los valores
+                divisibles por 2 y por 3 en el intervalo formado por los números ingresados. No realice ninguna suposición sobre el orden en 
+                el que se ingresan los valores y su magnitud.
+
+        */
+        static void Main(string[] args)
+        {
+            int valor1, valor2;
+
+            Console.WriteLine("ingrese valor 1:");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese valor 2:");
+            valor2 = int.Parse(Console.ReadLine());
+
+            //verifico que los intervalos sean crecientes
+            if(valor1 < valor2)
+            {
+                Console.Clear();
+                Console.WriteLine($"Numeros divicible por 2 en el intervalo {valor1} : {valor2}");
+                for(int i = valor1; i < valor2; i++)
+                {
+                    if(i%2 == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+               
+                }
+                Console.WriteLine($"\n Numeros divicible por 3 en el intervalo {valor1} : {valor2}");
+                for (int i = valor1; i < valor2; i++)
+                {
+
+                    if (i % 3 == 0)
+                    {
+                    Console.Write($"{i}  ");
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Numeros divicible por 2 en el intervalo {valor1} : {valor2}");
+                for (int i = valor2; i < valor1; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+
+                }
+                Console.WriteLine($"\n Numeros divicible por 3 en el intervalo {valor1} : {valor2}");
+                for (int i = valor2; i < valor1; i++)
+                {
+
+                    if (i % 3 == 0)
+                    {
+                        Console.Write($"{i}  ");
+                    }
+
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
